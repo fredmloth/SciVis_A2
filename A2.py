a3a3c2bdaf255196d6fb762fc0037776254e4c73
fredmloth <frederiekeloth@gmail.com>
2025-11-05 12:32:09 +0100

    places an inner temp

---
+++ A2.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# update all neighbours (at the same time, vectorize)
+def neighbour_update():
+
+    return
+
+# initialize system
+def initialise_system(n, inside, t_inner, t_top=0, t_tank=0):
+    # start grid
+    grid = np.zeros((n, n))
+
+    # set inner temp
+    start = (n - inside) // 2
+    end = start + inside
+    grid[start:end, start:end] = t_inner
+
+    # set top temp
+    #grid[] = t_top
+
+
+    # set tank temp
+    #grid[] = t_tank
+    return grid
+
+
+grid = initialise_system(4, 2, 2)
+print(grid)
