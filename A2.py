7951456b5cd9e52a1865566ec621a8d18208f695
fredmloth <frederiekeloth@gmail.com>
2025-11-05 12:46:24 +0100

    start work on class

--- A2.py
+++ A2.py
@@ -1,5 +1,27 @@
 import numpy as np
 
+class Grid():
+    def __init__(
+            self,
+            n=20,
+            inside=8,
+            h_tank=1,
+            t_inner=3,
+            t_top=2,
+            t_tank=1):
+        self.n=n
+        self.inside=inside
+        self.h_tank=h_tank
+        self.t_inner=t_inner
+        self.t_top=t_top
+        self.t_tank=t_tank
+
+
+
+        pass
+
+
+
 # update all neighbours (at the same time, vectorize)
 def neighbour_update():
 
@@ -23,6 +45,10 @@ def initialise_system(n=4, inside=2, h_tank=1, t_inner=3, t_top=2, t_tank=1):
     grid[bottom, 0:] = t_tank
     grid[bottom-h_tank:, 0] = t_tank
     grid[bottom-h_tank:, n-1] = t_tank
+
+    return grid
+
+def reset_temps(grid):
 
     return grid
 
