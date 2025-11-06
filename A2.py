197071f05716c820a368dfedd9c99d297b9141a7
fredmloth <frederiekeloth@gmail.com>
2025-11-06 13:53:27 +0100

    added mask for initial temperatures

--- A2.py
+++ A2.py
@@ -1,21 +1,28 @@
 import numpy as np
 
 class Grid():
+    # Define proportions
+    _BIG = 9
+    _CENTER = 3
+    _TANK = 4
+
     def __init__(
             self,
-            center=3,
-            h_tank=2,
-            t_inner=3,
-            t_top=2,
-            t_tank=1):
-        self.center=center
-        self.n=center*3
-        self.h_tank=h_tank
-        self.t_inner=t_inner
-        self.t_top=t_top
-        self.t_tank=t_tank
+            scale=1,
+            t_inner=212,
+            t_top=100,
+            t_tank=32):
+        
+        self.scale = scale
+        self.center = scale * self._CENTER
+        self.n = scale * self._BIG
+        self.h_tank = scale * self._TANK
+        self.t_inner = t_inner
+        self.t_top = t_top
+        self.t_tank = t_tank
 
         self.grid = self._init_grid()
+        self.mask = self.mask()
 
     def _init_grid(self):
         g = np.zeros((self.n, self.n))
@@ -35,9 +42,28 @@ class Grid():
         g[bottom-self.h_tank + 1:, self.n-1] = self.t_tank
 
         return g
+    
+    def mask(self):
+        m = np.zeros((self.n, self.n), dtype=bool)
+
+        # center
+        start = (self.n - self.center) // 2
+        end = start + self.center
+        m[start:end, start:end] = True
+
+        m[0, :] = True
+
+        bottom = self.n - 1
+        m[bottom, :] = True
+        m[bottom - self.h_tank + 1:, 0] = True
+        m[bottom - self.h_tank + 1:, -1] = True
+
+        return m
+
 
 g = Grid()
 print(g.grid)
+print(g.mask)
 
 
 
